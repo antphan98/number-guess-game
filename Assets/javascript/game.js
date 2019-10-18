22bc69f0847fe447c0ef8530176d22abdd5ed671
An Phan <antphan98@gmail.com>
2019-10-18 18:56:57 -0500

    created object to hold functions and variables

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -1,1 +1,31 @@
-alert("cu");
+// let randomNumber = Math.floor(Math.random() * 20) + 1;
+const numGuessGame = {
+    randomNumber: Math.floor(Math.random() * 20) + 1,
+    turns: 0,
+
+    
+    numCheck: function() {
+        document.getElementById("demo") = alert("hello");
+    
+      },
+
+
+}
+
+// document.getElementById("btn-primary").onclick = function() {
+//     alert("hello");
+
+// }
+
+
+//   if (turns === 0) {
+
+//     alert("You ran out of turns!");
+
+//   }
+
+//   function resetGame() {
+
+
+
+//   }
