759a2d20cdeb4f3b9b6e9f745687b486dd0f8fae
An Phan <antphan98@gmail.com>
2019-10-18 23:52:15 -0500

    added if else in userGuessCheck function

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -1,7 +1,8 @@
 // let randomNumber = Math.floor(Math.random() * 20) + 1;
 const numGuessGame = {
+
     randomNumber: Math.floor(Math.random() * 20) + 1,
-    turns: 0,
+    turns: 5,
 
     
     numCheck: function() {
@@ -9,8 +10,46 @@ const numGuessGame = {
     
       },
 
+    userGuessCheck: function(userGuess) {
+        while (userGuess = parseInt(userGuess)) {
+            this.turns--;
+
+            if (userGuess === this.randomNumber) {
+                alert("correct! it took " + this.turns + " amount of times");
+                return true;
+            }
+else if (guess > this.randomNumber) {
+    alert("guess too high");
+    this.turns--;
+    return false;
 
 }
+else {
+
+    alert("too low");
+    this.turns--;
+
+}
+
+
+        }
+
+
+
+    }
+
+      
+
+}
+
+
+function userNum() {
+	const userGuess = document.getElementById("button").value;
+    numGuessGame.userGuessCheck(userGuess);
+    
+}
+
+
 
 // document.getElementById("btn-primary").onclick = function() {
 //     alert("hello");
